528d9a5043ff70798b55fbed14a6d819e4cfccc8
Baek jiwoo <baekjiwoo.dev@gmail.com>
2025-07-09 21:44:37 +0900

    determine u2froot

--- crypto/bip39.c
+++ crypto/bip39.c
@@ -152,6 +152,10 @@ int mnemonic_to_bits(const char *mnemonic, uint8_t *bits) {
 }
 
 int mnemonic_check(const char *mnemonic) {
+  layoutDialogSwipe(
+                _("mnemonic check is excuted!"), NULL,
+                _("no"), _("ok!"), NULL,
+                NULL);
   uint8_t bits[32 + 1] = {0};
   int mnemonic_bits_len = mnemonic_to_bits(mnemonic, bits);
   if (mnemonic_bits_len != (12 * 11) && mnemonic_bits_len != (18 * 11) &&
