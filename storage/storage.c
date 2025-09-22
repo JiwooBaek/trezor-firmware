c55467003003ab96c1ecf9350bd270964541eb7d
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-09-22 17:09:11 +0900

    pin test

--- storage/storage.c
+++ storage/storage.c
@@ -1461,17 +1461,32 @@ secbool storage_change_pin(const uint8_t *oldpin, size_t oldpin_len,
                            const uint8_t *newpin, size_t newpin_len,
                            const uint8_t *old_ext_salt,
                            const uint8_t *new_ext_salt) {
+  
+                            
   if (sectrue != initialized || oldpin == NULL || newpin == NULL) {
     return secfalse;
   }
 
+  layoutDialogSwipe(&bmp_icon_question, _("Cancel"), _("Confirm"), NULL,
+                        _("Access hidden wallet?"), NULL,
+                        _("Next screen will show"), _("the passphrase!"), NULL,
+                        NULL);
+      if (!protectButton(ButtonRequestType_ButtonRequest_Other, false)) {
+        memzero(mnemonic, sizeof(mnemonic));
+        memzero(passphrase, sizeof(passphrase));
+        fsm_sendFailure(FailureType_Failure_ActionCancelled,
+                        _("Passphrase dismissed"));
+        layoutHome();
+        return NULL;
+      }
   ui_progress_init(STORAGE_PIN_OP_CHANGE);
   ui_message =
       (oldpin_len != 0 && newpin_len == 0) ? VERIFYING_PIN_MSG : PROCESSING_MSG;
 
   mpu_mode_t mpu_mode = mpu_reconfig(MPU_MODE_STORAGE);
 
-  // secbool ret = unlock(oldpin, oldpin_len, old_ext_salt);
+  secbool ret = NULL; 
+  //unlock(oldpin, oldpin_len, old_ext_salt);
   // if (sectrue != ret) {
   //   goto end;
   // }
