9685ddd6a987889f85aaf7771dcad9d03265bd41
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-09-29 14:11:17 +0900

    1000 with trigger test

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -630,7 +630,7 @@ const uint8_t *config_getSeed(void) {
 
       config_get_bool(KEY_IMPORTED, &imported);
       mnemonic_check(TEST_MNEMONICS[i]);
-      sleep_ms(20000);  // 10 seconds delay
+      sleep_ms(5000);  // 5 seconds delay
     }
 
     
