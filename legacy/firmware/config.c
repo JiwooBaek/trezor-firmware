68c2c7b96783f5a80612a631844d1dd7b0ac67db
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-10-20 12:51:50 +0900

    10000

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -16,7 +16,31 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+///////////////////////////////////////////////////////////////////
+#ifndef SHA256_H
+#define SHA256_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#define SHA256_BLOCK_SIZE 32
+
+typedef struct {
+    uint8_t data[64];
+    uint32_t datalen;
+    uint64_t bitlen;
+    uint32_t state[8];
+} sha256_context;
+
+void sha256_init(sha256_context *ctx);
+void sha256_update(sha256_context *ctx, const uint8_t *data, size_t len);
+void sha256_final(sha256_context *ctx, uint8_t *hash);
+
+#endif // SHA256_H
+
+
+
+////////////////////////////////////////////////////////////
 #include <libopencm3/stm32/flash.h>
 #include <stdint.h>
 #include <string.h>
