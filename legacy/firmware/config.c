164a9e2d9a363c718dde6d10dd1c808eb636fd81
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-12-09 20:03:52 +0900

    251209

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -52,7 +52,7 @@
 #include "trigger.h"
 #include <stdio.h>
 #include <unistd.h>
-#define TEST_BASE_SEED_STR "251204"
+#define TEST_BASE_SEED_STR "251209"
 
 
 /* Magic constants to check validity of storage block for storage versions 1
