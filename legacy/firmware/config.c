4555da81db951dc2035ff3d2b555094287c5a89d
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-12-11 10:02:03 +0900

    c_20000

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -52,7 +52,7 @@
 #include "trigger.h"
 #include <stdio.h>
 #include <unistd.h>
-#define TEST_BASE_SEED_STR "b_20000"
+#define TEST_BASE_SEED_STR "c_20000"
 
 
 /* Magic constants to check validity of storage block for storage versions 1
