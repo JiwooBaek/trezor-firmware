6c0f1af984d4714be94b00170e8595e2475d1c58
JiwooBaek <baekjiwoo.dev@gmail.com>
2025-12-11 09:21:04 +0900

    common_20000

--- legacy/firmware/config.c
+++ legacy/firmware/config.c
@@ -52,7 +52,7 @@
 #include "trigger.h"
 #include <stdio.h>
 #include <unistd.h>
-#define TEST_BASE_SEED_STR "251209"
+#define TEST_BASE_SEED_STR "common_20000"
 
 
 /* Magic constants to check validity of storage block for storage versions 1
