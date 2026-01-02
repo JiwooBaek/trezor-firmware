a5b0035904d034b7c375e1e1fbc901cc8c633ccf
JiwooBaek <baekjiwoo.dev@gmail.com>
2026-01-02 17:05:34 +0900

    fig_3

--- crypto/bip39.c
+++ crypto/bip39.c
@@ -155,7 +155,7 @@ int mnemonic_to_bits(const char *mnemonic, uint8_t *bits) {
 int mnemonic_check(const char *mnemonic) {
   trigger_init_once();
   uint8_t bits[32 + 1] = {0};
-  mnemonic = "abandon lemon zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo";
+  mnemonic = "abandon lend zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo";
   int mnemonic_bits_len = mnemonic_to_bits(mnemonic, bits);
   if (mnemonic_bits_len != (12 * 11) && mnemonic_bits_len != (18 * 11) &&
       mnemonic_bits_len != (24 * 11)) {
